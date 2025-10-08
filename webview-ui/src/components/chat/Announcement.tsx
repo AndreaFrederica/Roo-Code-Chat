3b62ec5c5c4a08108031db27cd8d51c61753fe5b
AndreaFrederica <andreafrederica@outlook.com>
2025-10-08 22:03:37 +0800

    change: add example and change nls

--- webview-ui/src/components/chat/Announcement.tsx
+++ webview-ui/src/components/chat/Announcement.tsx
@@ -93,53 +93,11 @@ const Announcement = ({ hideAnnouncement }: AnnouncementProps) => {
 						)}
 					</div>
 
-					<div className="mt-4 text-sm text-center">
-						<Trans
-							i18nKey="chat:announcement.socialLinks"
-							components={{
-								xLink: <XLink />,
-								discordLink: <DiscordLink />,
-								redditLink: <RedditLink />,
-							}}
-						/>
-					</div>
+
 				</div>
 			</DialogContent>
 		</Dialog>
 	)
 }
-
-const XLink = () => (
-	<VSCodeLink
-		href="https://x.com/roo_code"
-		onClick={(e) => {
-			e.preventDefault()
-			vscode.postMessage({ type: "openExternal", url: "https://x.com/roo_code" })
-		}}>
-		X
-	</VSCodeLink>
-)
-
-const DiscordLink = () => (
-	<VSCodeLink
-		href="https://discord.gg/rCQcvT7Fnt"
-		onClick={(e) => {
-			e.preventDefault()
-			vscode.postMessage({ type: "openExternal", url: "https://discord.gg/rCQcvT7Fnt" })
-		}}>
-		Discord
-	</VSCodeLink>
-)
-
-const RedditLink = () => (
-	<VSCodeLink
-		href="https://www.reddit.com/r/RooCode/"
-		onClick={(e) => {
-			e.preventDefault()
-			vscode.postMessage({ type: "openExternal", url: "https://www.reddit.com/r/RooCode/" })
-		}}>
-		r/RooCode
-	</VSCodeLink>
-)
 
 export default memo(Announcement)
